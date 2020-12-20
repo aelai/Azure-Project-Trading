474fed3ded4394d93730874d7329a0cb769fdd7b
aelai <alexanderelai@gmail.com>
2020-12-20 21:34:46 +0000

    UPDATE - modified ps script inc keyvault

--- PowerShell/InitiateStartUpDeployment.ps1
+++ PowerShell/InitiateStartUpDeployment.ps1
@@ -1,9 +1,43 @@
 [CmdletBinding()]
 param (
-    $SubscriptionID,
-    $SubscriptionName,
-    $Tenant
+    $FilePathStorage,
+    $FilePathKeyVault
 )
+#Authenticate azure account
 Connect-AzAccount
 
-Set-AzContext [ $SubscriptionID/$SubscriptionName/$Tenant]
+#Check if resource group already exists
+Get-AzResourceGroup -Name rgAzureProject -ErrorVariable notPresent -ErrorAction SilentlyContinue
+if ($notPresent)
+{
+    Write-Host "Creating new resource group"
+    New-AzResourceGroup `
+    -Name rgAzureProject `
+    -Location "Central US"
+}
+else
+{
+    Write-Host "Resource group rgAzureProject already exists."
+}
+
+Get-AzStorageAccount -Name adlsazureproject2020 -ErrorVariable notPresent -ErrorAction SilentlyContinue
+if ($notPresent)
+{  
+    Write-Host "Creating Storage Account adlsazureproject2020"
+    
+    #Deploy ARM template for storage
+    New-AzResourceGroupDeployment `
+    -Name Deploy_StartUp_StorageAccount `
+    -ResourceGroupName rgAzureProject `
+    -TemplateFile $FilePathStorage
+}
+else
+{
+    Write-Host "Resource group adlsazureproject2020 already exists."
+}
+
+#Deploy ARM template for key vault
+New-AzResourceGroupDeployment `
+-Name Deploy_StartUp_KeyVault `
+-ResourceGroupName rgAzureProject `
+-TemplateFile $FilePathKeyVault
